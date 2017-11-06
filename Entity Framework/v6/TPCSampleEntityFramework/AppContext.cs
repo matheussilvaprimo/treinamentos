c2f1ebadc0d056d965af5d0de62b639380ee597f
Matheus Primo <matheussilvaprimo@hotmail.com>
2017-11-06 16:37:39 -0200

    TPC Sample for Entity Framework 6

---
+++ Entity Framework/v6/TPCSampleEntityFramework/AppContext.cs
@@ -0,0 +1,36 @@
+﻿using System.Data.Entity;
+
+namespace TPCSampleEntityFramework
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Disciplina> Disciplinas { get; set; }
+        public DbSet<Professor> Professores { get; set; }
+
+        public AppContext() { }
+
+        public AppContext(string connString)
+            : base(connString)
+        {
+            Database.CreateIfNotExists();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Aluno>().Map(x =>
+            {
+                x.MapInheritedProperties();
+                x.ToTable("Alunos");
+            });
+
+            modelBuilder.Entity<Professor>().Map(x =>
+            {
+                x.MapInheritedProperties();
+                x.ToTable("Professores");
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
